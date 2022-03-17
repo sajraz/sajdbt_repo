a5d4cab8cf47e65e9e6ecb0ec75b3bc0fd0aa062
sajidaparveen.sk@techmahindra.com <sajidaparveen.sk@techmahindra.com>
2022-03-17 06:20:35 +0000

    cust_ord_details

---
+++ models/cust_ord/cust_ord_details.sql
@@ -0,0 +1,5 @@
+select c.id, user_id, full_name, order_date, status_code from 
+{{ ref('bv_customer') }}
+inner join
+{{ ref('bv_orders') }}
+on {{ ref('bv_customer') }}.id = {{ ref('bv_orders') }}.id
